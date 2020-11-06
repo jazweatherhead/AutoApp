48d3c5943105d3b8ba5a8645abb4135d319b88e2
Jaz <jaz@weatherheadco.de>
2020-11-06 14:38:17 -0700

    modelBuilder packing model

--- model/films.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const filmSchema = new mongoose.Schema({
-	title: { type: String, required: true},
-	director: { type: String, required: true},
-	year: { type: Number, required: true},
-})
-
-mongoose.model('Film', filmSchema) // the first param determines the collection name
