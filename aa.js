6d7eef7f1ed63301896e912c20d1e9a5285061be
Jaz <jaz@weatherheadco.de>
2020-11-05 23:43:40 -0700

    yarn dep test 5

--- aa.js
+++ aa.js
@@ -254,18 +254,18 @@ const exec = require('child_process').exec
 
 
 
-/* Installs the project dependencies */
-;(function installDeps (spinner) {
-  var exec = require('child_process').exec, child
-  // child = exec('npm i',
-  child = exec('yarn install',
-    function (err, stdout, stderr) {
-      console.log(`AA: built dependencies... ${stdout}`)
-      console.log(`AA: dependencies... ${stderr}`)
-      spinner.stop()
-      if (err !== null) {
-        console.log('exec error: ${err}')
-      }
-    }
-  )
-})(spinner)
+// /* Installs the project dependencies */
+// ;(function installDeps (spinner) {
+//   var exec = require('child_process').exec, child
+//   // child = exec('npm i',
+//   child = exec('yarn install',
+//     function (err, stdout, stderr) {
+//       console.log(`AA: built dependencies... ${stdout}`)
+//       console.log(`AA: dependencies... ${stderr}`)
+//       spinner.stop()
+//       if (err !== null) {
+//         console.log('exec error: ${err}')
+//       }
+//     }
+//   )
+// })(spinner)
